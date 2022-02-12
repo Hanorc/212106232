16c9ea2f2d77e6f12e88024b108168c7241afced
Hanorc <97528063+Hanorc@users.noreply.github.com>
2022-02-12 16:56:29 +0800

    Add files via upload

---
+++ SpeedKey.java
@@ -0,0 +1,8 @@
+package zheyun.speed;
+
+public class SpeedKey {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello World2");
+    }
+}
