bc7533759b3f8d60c31376bf57b2f9d1623a11cf
Hanorc <97528063+Hanorc@users.noreply.github.com>
2022-02-12 16:56:41 +0800

    Add files via upload

---
+++ VarableDemo1.java
@@ -0,0 +1,20 @@
+package zheyun.variable;
+
+public class VarableDemo1 {
+    public static void main(String[] args) {
+        //  目标：学会使用变量
+        // 数据类型 变量名称 = 初始值；
+        double money = 6.0;
+        System.out.println(money);
+
+        // 收红包(从右往左看)
+        money = money + 4.0;
+        System.out.println(money);
+        System.out.println("---------------------------------------------");
+        int age = 21;
+        System.out.println(age);
+
+        age = 25;
+        System.out.println(25);
+    }
+}
---
+++ VarableDemo2.java
@@ -0,0 +1,26 @@
+package zheyun.variable;
+
+public class VarableDemo2 {
+    public static void main(String[] args) {
+        // 目标：需要理解变量使用的注意事项
+//        1.变量要声明再使用。
+        int a = 23;
+        System.out.println(a);
+//        2.变量声明后，不能存储其他类型的数据。
+        // a = 1.5;
+//        3.变量的有效范围是从定义开始到"}"截止，且在同一个范围内部不能定义2个同名的变量。
+        {
+            int b = 25;
+            System.out.println(b);
+           //  int b = 100;// 报错
+        }
+        // System.out.println(b);
+        System.out.println(a);
+        // int a = 234 ;
+        a = 200; // 这里不是定义，在赋值
+//        4.变量定义的时候可以没有初始值，但是使用的时候必须给初始值。
+        int c;
+        c = 12;
+        System.out.println(c);
+    }
+}
